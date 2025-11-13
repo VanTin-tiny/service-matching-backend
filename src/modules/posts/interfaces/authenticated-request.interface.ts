2ae3dc93ddecd031115ccf271b1634ee26c19793
vantin <vvtin04@gmail.com>
2025-11-13 16:28:46 +0700

    oa oa oa

--- src/modules/posts/interfaces/authenticated-request.interface.ts
+++
@@ -1,6 +0,0 @@
-import { JwtPayload } from '@/modules/auth/interfaces/jwt-payload.interface';
-import { Request } from 'express';
-
-export interface AuthenticatedRequest extends Request {
-    user: JwtPayload;
-}
