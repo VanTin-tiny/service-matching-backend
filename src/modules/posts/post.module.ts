edd18b39aa0d3a36b027713bab07ef54cade1d93
vantin <vvtin04@gmail.com>
2025-11-13 15:34:54 +0700

    :))

--- src/modules/posts/post.module.ts
+++ src/modules/posts/post.module.ts
@@ -1,14 +1,14 @@
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { PostController } from './controllers/post.controller';
-import { PostCustomer } from './entities/post.entity';
-import { PostRepository } from './repositories/post.repository';
-import { PostService } from './services/post.service';
+// import { Module } from '@nestjs/common';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { PostController } from './controllers/post.controller';
+// import { PostCustomer } from './entities/post.entity';
+// import { PostRepository } from './repositories/post.repository';
+// import { PostService } from './services/post.service';
 
-@Module({
-    imports: [TypeOrmModule.forFeature([PostCustomer])],
-    controllers: [PostController],
-    providers: [PostService, PostRepository],
-    exports: [PostService],
-})
-export class PostModule { }
+// @Module({
+//     imports: [TypeOrmModule.forFeature([PostCustomer])],
+//     controllers: [PostController],
+//     providers: [PostService, PostRepository],
+//     exports: [PostService],
+// })
+// export class PostModule { }
